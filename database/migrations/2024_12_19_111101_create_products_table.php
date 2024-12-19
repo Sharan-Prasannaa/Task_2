a95d543dc33312dde75e9b5f16d3086cdee9a904
Your Name <you@example.com>
2024-12-19 17:09:23 +0530

    Created Migrations

--- database/migrations/2024_12_19_111101_create_products_table.php
+++ database/migrations/2024_12_19_111101_create_products_table.php
@@ -11,12 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->enum('role',['admin','manager','customer'])->default('customer');
+            $table->text('description');
+            $table->decimal('price',10,2);
+            $table->integer('stock');
+            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
+            $table->foreignId('supplier_id')->constrained('suppliers')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -26,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 };
--- database/migrations/2024_12_19_111132_create_orders_table.php
+++ database/migrations/2024_12_19_111132_create_orders_table.php
@@ -11,12 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->enum('role',['admin','manager','customer'])->default('customer');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->date('order_date');
+            $table->decimal('total_amount', 8, 2);
+            $table->enum('status', ['pending', 'completed', 'cancelled']);
             $table->timestamps();
         });
     }
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('orders');
     }
 };
